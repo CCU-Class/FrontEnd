bd7cb0b03266d494fea86435854a6c620ae45adb
as6325400 <as6325400@gmail.com>
2023-08-14 03:28:04 +0800

    add animate

--- src/components/search_box.vue
+++ src/components/search_box.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full top-8 h-8 flex sticky" draggable="true">
-        <div class = "ml-auto text-orange-400 w-8 text-3xl" ref="toolbox" v-on:click="open_search()">
-            <SearchOutlined ref="searchBox" />
+        <div class = "ml-auto text-orange-400 w-8 text-3xl relative small  animate" ref="toolbox" v-on:click="open_search()">
+            <SearchOutlined class= "absolute left-0 top-0" ref="searchBox" />
         <!-- <img src = "/search_box_icon.png" class = "ml-auto"> -->
         </div>
     </div>
@@ -18,20 +18,30 @@ let toolbox = ref(null);
 let open_search = function(){
     if(count.value == 0){
         searchBox.value.classList.add('hidden');
-        toolbox.value.classList.remove('w-8');
-        toolbox.value.classList.add('w-28');
-        toolbox.value.classList.add('h-28');
-        toolbox.value.classList.add('bg-black');
+        toolbox.value.classList.add('large');
         count.value = 1;
     }
     else{
+        toolbox.value.classList.remove('large');
         searchBox.value.classList.remove('hidden');
-        toolbox.value.classList.remove('bg-black');
-        toolbox.value.classList.add('w-8');
-        toolbox.value.classList.remove('w-28');
-        toolbox.value.classList.remove('h-28');
         count.value = 0;
     }
 }
 
 </script>
+
+<style>
+    .animate{
+        transition: transform 0.3s ease;
+    }
+    .small{
+        position: relative;
+        background-color: white;
+        border-radius: 0.25rem;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+    .large{
+        transform: scale(10, 14) translateY(15px) translateX(-15px);
+        border-radius: 0.15rem;
+    }
+</style>
