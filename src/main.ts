b6ebadd84e5cdfa33493d3b5a748e182dbf4bee4
syzygy608 <hankwu99@gmail.com>
2023-08-13 10:11:21 +0800

    add icon

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './css/tailwind.css';
+import * as Icons from '@ant-design/icons-vue';
 
 const app = createApp(App);
+
+const icons: any = Icons;
+
+for (const i in icons) {
+  app.component(i, icons[i]);
+}
+
 app.use(router);
 app.mount('#app');
 
