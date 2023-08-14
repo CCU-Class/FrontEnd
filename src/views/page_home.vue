d2b7bb61ce2d6db084a749585ee0b1b47b0a37df
as6325400 <as6325400@gmail.com>
2023-08-14 12:05:54 +0800

    change content

--- src/views/page_home.vue
+++ src/views/page_home.vue
@@ -11,10 +11,10 @@ import Announce from '../components/announcement.vue';
         <div class = "mx-auto py-12 bg-slate-100">
                 <div class = 'w-full px-3 md:w-8/12 mb-3 mx-auto tracking-tight'>
                     <div class = 'bg-gradient-to-br from-orange-300 to-red-700 text-transparent bg-clip-text font-semibold text-3xl'>
-                        CCU Pineapple Schedule
+                        CCU Class
                     </div>
                     <div class = 'font-semibold text-3xl'>
-                        鳳梨課表
+                        中正課表
                     </div>
                     <div class = 'text-xl text-gray-500 my-1'>
                         歡迎來到專屬於所有中正人的排課與課表製作網頁服務
