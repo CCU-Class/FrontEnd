346274cdd83ff2c4f8038e67a3ed18d87342e71b
as6325400 <as6325400@gmail.com>
2023-08-14 14:54:35 +0800

    fix animate

--- src/components/search_box.vue
+++ src/components/search_box.vue
@@ -1,47 +1,57 @@
 <template>
     <div class="w-full top-8 h-8 flex sticky" draggable="true">
-        <div class = "ml-auto text-orange-400 w-8 text-3xl relative small  animate" ref="toolbox" v-on:click="open_search()">
-            <SearchOutlined class= "absolute left-0 top-0" ref="searchBox" />
-        <!-- <img src = "/search_box_icon.png" class = "ml-auto"> -->
+        <div
+            class="ml-auto text-orange-400 w-8 text-3xl relative small animate"
+            ref="toolbox"
+            @click="openSearch"
+            @transitionend="transitionend"
+        >
+            <SearchOutlined class="absolute left-0 top-0" ref="searchBox" />
+            <!-- <img src = "/search_box_icon.png" class = "ml-auto"> -->
         </div>
     </div>
-    <div>
-    </div>
-</template> 
+</template>
 
 <script setup>
-import {ref } from "vue";
+import { ref } from "vue";
 
-let count = ref(0);
-let searchBox = ref(null);
-let toolbox = ref(null);
-let open_search = function(){
-    if(count.value == 0){
-        searchBox.value.classList.add('hidden');
-        toolbox.value.classList.add('large');
+const count = ref(0);
+const searchBox = ref(null);
+const toolbox = ref(null);
+
+const openSearch = () => {
+    if (count.value === 0) {
+        searchBox.value.classList.add("hidden");
+        toolbox.value.classList.add("large");
         count.value = 1;
-    }
-    else{
-        toolbox.value.classList.remove('large');
-        searchBox.value.classList.remove('hidden');
+    } else {
+        toolbox.value.classList.remove("large");
         count.value = 0;
     }
-}
+};
+
+const transitionend = () => {
+    if (count.value == 0) {
+        searchBox.value.classList.remove("hidden");
+    } else {
+        
+    }
+};
 
 </script>
 
 <style>
-    .animate{
-        transition: transform 0.3s ease;
-    }
-    .small{
-        position: relative;
-        background-color: white;
-        border-radius: 0.25rem;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    }
-    .large{
-        transform: scale(10, 14) translateY(15px) translateX(-15px);
-        border-radius: 0.15rem;
-    }
+.animate {
+    transition: transform 0.3s ease;
+}
+.small {
+    position: relative;
+    background-color: white;
+    border-radius: 0.25rem;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+.large {
+    transform: scale(10, 14) translateY(15px) translateX(-15px);
+    border-radius: 0.10rem;
+}
 </style>
