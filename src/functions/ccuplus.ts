c9ecd7325fb9b8262dd55e6a655458ab42df7fe6
as6325400 <as6325400@gmail.com>
2023-08-24 22:08:49 +0800

    add https request to ccuplus

---
+++ src/functions/ccuplus.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+const env = import.meta.env;
+const apiSite = `https://${env.VITE_CCUPLUS_DEVICE}/`;
+
+
+export async function searchCourseOnCcuplus(course_id: string) {
+  const apiUrl = apiSite + course_id;
+  
+  return new Promise((resolve, reject) => {
+      axios.get(apiUrl, {
+      })
+      .then((response) => {
+          resolve(response.data);
+      })
+      .catch((error) => {
+          console.error(error);
+          reject(error);
+      });
+  });
+}
+
+export async function searchCommentsOnCcuplus(course_id: string) {  
+  const apiUrl = apiSite + course_id + "comments";
+  return new Promise((resolve, reject) => {
+      axios.get(apiUrl, {
+      })
+      .then((response) => {
+          resolve(response.data);
+      })
+      .catch((error) => {
+          console.error(error);
+          reject(error);
+      });
+  });
+}
