2bd88ba03ebb8f8977ba3d277feaa1e96f0cd57c
syzygy608 <hankwu99@gmail.com>
2023-08-27 16:42:00 +0800

    split page finish

--- src/views/page_main.vue
+++ src/views/page_main.vue
@@ -5,19 +5,31 @@ import classTable from '@components/pages/main/classTable.vue';
 import Box from '@components/pages/main/search_box.vue';
 import comment from '@components/pages/main/comment.vue';
 
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+import { useStore } from 'vuex';
+import { computed } from 'vue';
 
+const store = useStore();
+const status = computed(() => store.state.show);
 
 </script>
 
 <template>
     <div id = "main" class="flex relative">
+        
         <div>
             <Navbar/>
             <Box/>
-            <classTable/>
-            <Foot/>
+            <splitpanes class = "bg-white">
+                <pane class = "w-full" min-size = "40">
+                    <classTable/>
+                </pane>
+                <pane v-if = "status" min-size = "30">
+                    <comment />
+                </pane>
+            </splitpanes>
+            <Foot/> 
         </div>
-        <!-- <div id="box"></div> -->
-        <!-- <comment/> -->
     </div>
 </template>
