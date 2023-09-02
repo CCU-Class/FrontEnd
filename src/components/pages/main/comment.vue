b8205f31a1eacde619e7c51d05476398dae19c33
syzygy608 <hankwu99@gmail.com>
2023-09-02 18:55:32 +0800

    add more layer to comment

--- src/components/pages/main/comment.vue
+++ src/components/pages/main/comment.vue
@@ -5,14 +5,14 @@
             Powered by
             <img src = "/ccuplus.svg" class = "w-1/2 mx-auto my-3">
         </a>
-        <button v-on:click = "hidden" class = 'btn-comment mx-3'>關閉評價</button>
+        <button v-on:click = "hidden" class = 'btn-comment mx-3' id="close">關閉評價</button>
         <p v-if = "comment_status" class = "text-base">
             <p class = "text-green-200 px-3 pt-2">
-                因篇幅關係，本介面僅顯示最頂層的評論，<br>
+                因篇幅關係，本介面僅顯示前四層的評論，<br>
                 若有需要請至最下方的查看更多查看評價之留言。
             </p>
-            <div v-for = "comment in comments" class = "comment my-2 mx-3 un">
-                <div v-if = " comment.deleted" class = "text-red-300">此評價已被刪除</div>
+            <div v-for = "comment in comments" class = "comment my-6 mx-3">
+                <div v-if = "comment.deleted" class = "text-red-300">此評價已被刪除</div>
                 <div v-else class = "comment_content">
                     <div class = "comment_title text-xl font-bold text-left text-green-200">
                         <div v-if = "comment.user"> {{ comment.user }}:</div>
@@ -20,16 +20,67 @@
                         <hr class = "bg-black">
                     </div>
                     <div class = "py-2 px-2 font-serif text-white text-left whitespace-pre-line">{{ comment.content }}</div>
-                    <div class = "text-gray-300 text-sm">
+                    <div class = "text-gray-300 text-sm text-right">
                         <span>受評教師: {{ comment.professor }}</span>
                         <span> | </span> 
                         <span>評價時間: {{ comment.commented_at }}</span>
                     </div>
                 </div>
+                <div v-if = "comment.comments.length">
+                    <div v-for = "subcomment in comment.comments" class = "comment py-2 px-3 bg-gray-600/50">
+                        <div v-if = "subcomment.deleted" class = "text-red-300">此評價已被刪除</div>
+                        <div v-else class = "comment_content">
+                            <div class = "comment_title text-xl font-bold text-left text-green-200">
+                                <div v-if = "subcomment.user"> {{ subcomment.user }}:</div>
+                                <div v-else>匿名: </div>
+                                <hr class = "bg-black">
+                            </div>
+                            <div class = "py-2 px-2 font-serif text-white text-left whitespace-pre-line">{{ subcomment.content }}</div>
+                            <div class = "text-gray-300 text-sm text-right">
+                                <span>評價時間: {{ subcomment.commented_at }}</span>
+                            </div>
+                        </div>
+                        <div v-if = "subcomment.comments.length">
+                            <div v-for = "subsubcomment in subcomment.comments" class = "comment my-2 ml-6 bg-gray-600/50 px-2">
+                                <div v-if = "subsubcomment.deleted" class = "text-red-300">此評價已被刪除</div>
+                                <div v-else class = "comment_content">
+                                    <div class = "comment_title text-xl font-bold text-left text-green-200">
+                                        <div v-if = "subsubcomment.user"> {{ subsubcomment.user }}:</div>
+                                        <div v-else>匿名: </div>
+                                        <hr class = "bg-black">
+                                    </div>
+                                    <div class = "py-2 px-2 font-serif text-white text-left whitespace-pre-line">{{ subsubcomment.content }}</div>
+                                    <div class = "text-gray-300 text-sm text-right">
+                                        <span>評價時間: {{ subsubcomment.commented_at }}</span>
+                                    </div>
+                                </div>
+                                <div v-if = "subsubcomment.comments.length">
+                                    <div v-for = "subsubsubcomment in subsubcomment.comments" class = "comment my-2 ml-6 px-2 py-2 bg-gray-600/50">
+                                        <div v-if = "subsubsubcomment.deleted" class = "text-red-300">此評價已被刪除</div>
+                                        <div v-else class = "comment_content">
+                                            <div class = "comment_title text-xl font-bold text-left text-green-200">
+                                                <div v-if = "subsubsubcomment.user"> {{ subsubsubcomment.user }}:</div>
+                                                <div v-else>匿名: </div>
+                                                <hr class = "bg-black">
+                                            </div>
+                                            <div class = "py-2 px-2 font-serif text-white text-left whitespace-pre-line">{{ subsubsubcomment.content }}</div>
+                                            <div class = "text-gray-300 text-sm text-right">
+                                                <span>評價時間: {{ subsubsubcomment.commented_at }}</span>
+                                            </div>
+                                        </div>
+                                        <div v-if = "subsubsubcomment.comments.length">
+                                            更多留言請上 <a :href = "link" target = "_blank" class = "text-green-200">ccu.plus</a> 查看
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <a :href = "link" target = "_blank" class = "btn-comment my-3 py-1">查看更多</a>
         </p>
-        <p v-else class = "text-green-100 font-extrabold text-2xl my-3 mx-3">此門課查無評價</p> 
+        <p v-else class = "text-green-100 font-extrabold text-2xl my-3 mx-3">此門課查無評價或發生問題</p> 
     </div>
 </template>
 
