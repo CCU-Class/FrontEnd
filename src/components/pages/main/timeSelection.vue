a147dc8a99304b3f488ecf72c412d9638e15be26
syzygy608 <hankwu99@gmail.com>
2023-12-03 21:34:49 +0800

    add spinner

--- src/components/pages/main/timeSelection.vue
+++ src/components/pages/main/timeSelection.vue
@@ -2,15 +2,16 @@
     <div class="z-30 top-0 left-0 w-screen h-screen fixed flex items-center backdrop-blur-sm" v-show="status">
         <div class="h-4/6 w-full md:w-6/12 mx-auto bg-white px-3 py-2 rounded-lg drop-shadow-xl"> 
             <div class="px-6 py-6">
-                <div class="flex justify-between border-b border-b-purple-400 pb-2 mb-2">
-                    <div class="text-2xl font-bold text-purple-500">課程時間搜尋</div>
+                <div class="flex justify-between border-b border-b-orange-200 pb-2 mb-2">
+                    <div class="text-2xl font-bold text-orange-300">課程時間搜尋</div>
                     <CloseCircleOutlined class="text-2xl font-bold cursor-pointer text-purple-900" @click="close"/>
                 </div>
                 <!-- <div class = "flex flex-row text-md">
                     <p class = "font-bold"></p>
                     <p class = "mx-2"></p>
                 </div> -->
-                <div class="max-h-[28rem] overflow-x-hidden overflow-y-auto mt-6">
+                <div class="overflow-x-hidden mt-6 overflow-y-auto h-80">
+                    <loadingSpinner v-if="isLoading" style="height: auto;"></loadingSpinner> 
                     <div v-for="item in search_class_list_in_timemode" class="w-full bg-white/70 px-1 py-1 hover:bg-orange-300 hover:text-white cursor-pointer border-2" 
                     :class="{conflict: item.conflict}">
                         {{item.class_name}} {{item.teacher}} {{item.class_time}} 
@@ -34,9 +35,12 @@ const status = computed(() => store.state.course.searchTime_status);
 const searchTimeArgument = computed(() => store.state.course.timeSearchArgument);
 let search_class_list_in_timemode = ref([]);
 
+const isLoading = ref(false);
+
 watch(searchTimeArgument, async () => {
     console.log(searchTimeArgument.value);
     search_class_list_in_timemode.value = [];
+    isLoading.value = true;
     let data = await searchCourseByTime(searchTimeArgument.value[0], searchTimeArgument.value[1], searchTimeArgument.value[2]);
     console.log(data);
     search_class_list_in_timemode.value = [];
@@ -46,6 +50,7 @@ watch(searchTimeArgument, async () => {
         temp['conflict'] = conflict;
         search_class_list_in_timemode.value.push(temp);
     }
+    isLoading.value = false;
 })
 
 const close = () => {
