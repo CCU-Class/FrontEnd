1ed21a9d8cbc1b47d5cf146653ca01a09b851085
as6325400 <as6325400@gmail.com>
2023-12-21 18:08:54 +0800

    fix time tool

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ASwitch: typeof import('ant-design-vue/es')['Switch']
     ClassTable: typeof import('./src/components/pages/main/classTable.vue')['default']
     ClipLoadingSpinner: typeof import('./src/components/common/ClipLoadingSpinner.vue')['default']
     ColorTemplate: typeof import('./src/components/pages/main/colorTemplate.vue')['default']
