1d08b887d32e6308caac7017375db6fb779da51e
syzygy608 <hankwu99@gmail.com>
2024-01-17 11:01:48 +0800

    fix some bug and css

--- src/components/pages/main/serach_modes/department.vue
+++ src/components/pages/main/serach_modes/department.vue
@@ -3,27 +3,37 @@
         <div class = 'w-2/12 mx-3 py-2 font-semibold min-w-[4rem] -order-1 text-red-500'>
             以系所年級進行搜尋
         </div>
-        <div class="flex px-4">
-            <div class="w-3/12">
-                <input class = 'mx-auto py-1 text-center course_search' type = "search" placeholder = "系所" v-model = "departmentInput">
-                <ul class = "mx-auto w-11/12 result-show overflow-y-auto overflow-x-hidden" ref = "department_search_list" v-show="show_department">
-                    <li v-for="item in department_data" class = "w-full bg-white/70 px-1 py-1 hover:bg-orange-300 hover:text-white" 
-                    @click="departmentInput = item, departmentflag = false, clickDepartment()">
-                        {{ item }}
-                    </li>
-                </ul>
+        <div>
+            <div class = "w-full flex justify-between">
+                <div class="mx-12 w-4/12">
+                    <input class = 'w-full py-1 text-center' type = "search" placeholder = "系所" v-model = "departmentInput">
+                    <ul class = "mx-auto result-show overflow-y-auto overflow-x-hidden z-10" ref = "department_search_list" v-show="show_department">
+                        <li v-for="item in department_data" class = "w-full bg-white/70 px-3 py-1 hover:bg-orange-300 hover:text-white" 
+                        @click="departmentInput = item, departmentflag = false, clickDepartment()">
+                            {{ item }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="mx-12 w-4/12 inline-grid grid-cols-2">
+                    <div class = "text-center leading-8">
+                        年級 / 向度
+                    </div>
+                    <select class = 'w-full h-8 text-center' v-model = "gradeSelection">
+                        <option v-for="(option, index) in gradeList" :key="index" :value="option.grade"> {{ option.grade }} </option>
+                    </select>
+                </div>
             </div>
-            <div class="w-1/12">
-                <select class = 'w-full h-8 text-center' v-model = "gradeSelection">
-                    <option v-for="(option, index) in gradeList" :key="index" :value="option.grade"> {{ option.grade }} </option>
-                </select>
-            </div>
-            <div class="w-full">
-                <ul class = "mx-auto w-11/12 result-show overflow-y-auto overflow-x-hidden" ref="searchList">
-                    <li v-for = "item in filteredClassList" class = "w-full bg-white/70 px-1 py-1 hover:bg-orange-300 hover:text-white" :class="{conflict: item.conflict}" @click="push_to_table(2, item) && addConflict(item)">
+            <div class="w-full -z-40 mt-3">
+                <ul class = "mx-auto w-11/12 result-show overflow-y-auto overflow-x-hidden" ref="searchList" v-show="show_search_list">
+                    <li v-for = "item in filteredClassList" class = "w-full bg-white/70 px-6 py-1 hover:bg-orange-300 hover:text-white" :class="{conflict: item.conflict}" @click="push_to_table(2, item) && addConflict(item)">
                         [{{item.id}}] {{item.class_name}} {{item.teacher}} {{item.class_time}} {{item.class_room}} 
                     </li>
                 </ul>
+                <div class = "mx-auto w-11/12 h-36" v-show = "!show_search_list">
+                    <div class = "w-full h-full flex justify-center items-center text-2xl bg-white">
+                        
+                    </div>
+                </div>
             </div>
         </div>
         
@@ -44,7 +54,7 @@ const store = useStore();
 const setSearchTimeMode = (flag) => store.dispatch("setTimeSearchMode", flag);
 const isLoading = ref(false);
 const isInputEmpty = ref(false);
-const show_search_box = ref(false);
+const show_search_list = ref(false);
 const data = ref([]);
 const searchList = ref(null);
 let departmentList = [];
@@ -70,16 +80,13 @@ watch(departmentInput, async (inputValue) => {
     {   
         show_department.value = true;
         department_data.value = departmentList.filter(name => name.includes(inputValue));
-        if(searchList != null)
-        {   
-            // ul's max-height is 2rem x env.VITE_UL_ROW
-            department_search_list.value.style.maxHeight = (2 * env.VITE_UL_ROW).toString() + "rem";
-        }
     }
     else
     {
         show_department.value = false;
+        show_search_list.value = false;
         departmentflag.value = true;
+        gradeList.value = [];
     }
 });
 
@@ -99,6 +106,7 @@ async function clickDepartment()
         temp['conflict'] = classconflict(temp);
         return temp;
     })
+    show_search_list.value = true;
 }
 
 function addConflict(item){
@@ -113,7 +121,7 @@ onMounted(async () =>
         searchList.value.style.maxHeight = (2 * env.VITE_UL_ROW).toString() + "rem";
         department_search_list.value.style.maxHeight = (2 * env.VITE_UL_ROW).toString() + "rem";
     }
-    departmentList= await getDepartment();
+    departmentList = await getDepartment();
     setSearchTimeMode(false);
 })
 </script>
