96bbf375708f0c5915382431cfc904fdba8b47dc
as6325400 <as6325400@gmail.com>
2024-01-29 18:53:04 +0800

    add prettierrc

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -4,31 +4,22 @@ const router = useRouter();
 </script>
 
 <template>
-    <div class = "z-10 bg-orange-200 px-3 py-3 flex md:flex-row flex-row-reverse items-center place-content-between select-none">
-        <router-link to = "/">
-            <div class = "font-bold text-2xl bg-gradient-to-br from-orange-300 to-red-700 text-transparent bg-clip-text">
-                中正課表
-            </div>
-        </router-link>
-        <div class = "text-md hidden md:block">
-            <router-link to = "/" class = "btn-head">
-                首頁
-            </router-link>
-            <router-link to = "/tutorial" class = "btn-head">
-                使用教學
-            </router-link>
-            <router-link to = "/main" class = "btn-head">
-                開始使用
-            </router-link>
-        </div>
-        <div class = 'block md:hidden'>
-            <!-- navbar collapse icon -->
-            <button type = 'button' class = 'py-2 px-2 rounded text-black' v-on:click = "collapse">
-                <svg xmlns = "http://www.w3.org/2000/svg" class = "h-6 w-6" fill = "none" viewBox = "0 0 24 24" stroke = "currentColor" stroke-width = "2">
-                    <path stroke-linecap = "round" stroke-linejoin = "round" d = "M4 6h16M4 10h16M4 14h16M4 18h16"/>
-                </svg>
-            </button>
-        </div>
+  <div
+    class="z-10 bg-orange-200 px-3 py-3 flex md:flex-row flex-row-reverse items-center place-content-between select-none">
+    <router-link to="/">
+      <div
+        class="font-bold text-2xl bg-gradient-to-br from-orange-300 to-red-700 text-transparent bg-clip-text">
+        中正課表
+      </div>
+    </router-link>
+    <div class="text-md hidden md:block">
+      <router-link to="/" class="btn-head"> 首頁 </router-link>
+      <router-link to="/tutorial" class="btn-head">
+        使用教學
+      </router-link>
+      <router-link to="/main" class="btn-head">
+        開始使用
+      </router-link>
     </div>
     <div class="block md:hidden">
       <!-- navbar collapse icon -->
@@ -50,6 +41,26 @@ const router = useRouter();
         </svg>
       </button>
     </div>
+  </div>
+  <div class="block md:hidden">
+    <!-- navbar collapse icon -->
+    <button
+      type="button"
+      class="py-2 px-2 rounded text-black"
+      v-on:click="collapse">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+      </svg>
+    </button>
   </div>
   <div
     class="z-0 block w-full bg-orange-200 md:hidden"
