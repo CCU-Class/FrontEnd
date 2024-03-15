a45055c3ae38012200675bfd0642b82871d5b6d8
syzygy608 <hankwu99@gmail.com>
2024-03-15 13:38:40 +0800

    fix hamburger

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -42,26 +42,6 @@ const router = useRouter();
       </button>
     </div>
   </div>
-  <div class="block md:hidden">
-    <!-- navbar collapse icon -->
-    <button
-      type="button"
-      class="py-2 px-2 rounded text-black"
-      v-on:click="collapse">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M4 6h16M4 10h16M4 14h16M4 18h16" />
-      </svg>
-    </button>
-  </div>
   <div
     class="z-0 block w-full bg-orange-200 md:hidden"
     v-if="view === 2">
