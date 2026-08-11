5f32b9dcc9b5d45e1f0a51f05357be7e8a806f7d
yudong-0222 <yuchi9716@gmail.com>
2026-08-11 17:15:22 +0800

    style: improve responsive layout for custom course form

--- src/components/pages/main/serach_modes/custom.vue
+++ src/components/pages/main/serach_modes/custom.vue
@@ -1,47 +1,70 @@
 <template>
-  <div
-    class="w-2/12 mx-3 py-2 font-semibold min-w-[4rem] -order-1 text-pink-500">
+  <div class="w-2/12 mx-3 py-2 font-semibold -order-1 text-pink-500">
     自定義新增課程
   </div>
-  <div class="flex flex-row mx-auto w-full">
-    <div class="mx-3 py-1 font-semibold min-w-[4rem]">課程資訊</div>
-    <input
-      class="mx-2 w-full md:w-5/12 py-1 rounded-md text-center"
-      type="text"
-      placeholder="課程名稱"
-      v-model="className" />
-    <input
-      class="mx-2 w-full md:w-3/12 py-1 rounded-md text-center"
-      type="text"
-      placeholder="課程教室"
-      v-model="classRoom" />
-    <select
-      class="mx-1 py-1 rounded-md text-center"
-      v-model="weekDay">
-      <option selected>星期</option>
-      <option v-for="day in week" :value="day">{{ day }}</option>
-    </select>
-    <select class="mx-1 py-1 rounded-md text-center" v-model="start">
-      <option selected>始堂</option>
-      <option v-for="cla in classes" :value="cla">{{ cla }}</option>
-    </select>
-    <select class="mx-1 py-1 rounded-md text-center" v-model="end">
-      <option selected>終堂</option>
-      <option v-for="cla in classes" :value="cla">{{ cla }}</option>
-    </select>
-    <button
-      class="btn-normal"
-      v-on:click="
-        push_to_table(1, {
-          className: className,
-          classRoom: classRoom,
-          weekDay: weekDay,
-          start: start,
-          end: end,
-        })
-      ">
-      +
-    </button>
+  <div
+    class="flex flex-col md:flex-row mx-auto w-full gap-2 md:gap-0">
+    <!-- 第一列：課程資訊 [課程名稱] [課程教室] -->
+    <div class="flex flex-row w-full md:w-auto">
+      <div class="mx-3 py-1 font-semibold min-w-[4rem]">課程資訊</div>
+
+      <input
+        class="mx-1 md:mx-2 flex-1 md:w-5/12 py-1 rounded-md text-center"
+        type="text"
+        placeholder="課程名稱"
+        v-model="className" />
+
+      <input
+        class="mx-1 md:mx-2 flex-1 md:w-3/12 py-1 rounded-md text-center"
+        type="text"
+        placeholder="課程教室"
+        v-model="classRoom" />
+    </div>
+
+    <!-- 第二列：星期、始堂、終堂、+ -->
+    <div
+      class="flex flex-row w-full md:w-auto justify-end md:justify-start">
+      <select
+        class="mx-1 py-1 rounded-md text-center flex-1 md:flex-none"
+        v-model="weekDay">
+        <option selected>星期</option>
+        <option v-for="day in week" :value="day">
+          {{ day }}
+        </option>
+      </select>
+
+      <select
+        class="mx-1 py-1 rounded-md text-center flex-1 md:flex-none"
+        v-model="start">
+        <option selected>始堂</option>
+        <option v-for="cla in classes" :value="cla">
+          {{ cla }}
+        </option>
+      </select>
+
+      <select
+        class="mx-1 py-1 rounded-md text-center flex-1 md:flex-none"
+        v-model="end">
+        <option selected>終堂</option>
+        <option v-for="cla in classes" :value="cla">
+          {{ cla }}
+        </option>
+      </select>
+
+      <button
+        class="btn-normal mx-1"
+        v-on:click="
+          push_to_table(1, {
+            className: className,
+            classRoom: classRoom,
+            weekDay: weekDay,
+            start: start,
+            end: end,
+          })
+        ">
+        +
+      </button>
+    </div>
   </div>
 </template>
 
